67f7d485f8d863dc0f8db6ae1456af4e114d4116
stvian <76795576+istvian@users.noreply.github.com>
2025-10-08 09:09:50 -0300

    Add files via upload

--- 2° Semestre/Programacion/ProyectoDB/Conecta.py
+++ 2° Semestre/Programacion/ProyectoDB/Conecta.py
@@ -8,6 +8,8 @@ while True:
         "\nElija una opción:\n\
                   \tMostrar un repuesto(u)\n\
                   \tMostrar todos los repuestos(t)\n\
+                  \tMostrar todas las ventas(vv)\n\
+                  \tMostrar una venta(v)\n\
                   \tFin(f)\n\
                   \t=>".lower()
     )
@@ -16,6 +18,11 @@ while True:
         db.select_uno(codABuscar)
     elif elige == "t":
         db.select_todos()
+    elif elige == "vv":
+        db.venta_todos()
+    elif elige == "v":
+        codABuscar = input("Ingrese código a buscar=")
+        db.venta_uno(codABuscar)
     elif elige == "f":
         print("Fin")
         db.cerrarDB()
--- 2° Semestre/Programacion/ProyectoDB/Database.py
+++ 2° Semestre/Programacion/ProyectoDB/Database.py
@@ -48,3 +48,40 @@ class Database:
                 print("Código no existe")
         except Exception as err:
             print(err)
+
+    def venta_todos(self):
+        sql = "SELECT * FROM VENTAS"
+        try:
+            self.cursor.execute(sql)
+            repu = self.cursor.fetchall()
+            print(
+                f"{'NúmVenta':10}{'Cod repuesto':20}{'Fecha vent.':12}\
+                  {'Monto vent':12}"
+            )
+            for rep in repu:
+                print(
+                    f"{rep[0]:<10}{rep[1]:20}{rep[2].strftime('%d/%m/%Y'):12}\
+                      {rep[3]:<12}"
+                )
+                # print(rep)
+        except Exception as err:
+            print(err)
+
+    def venta_uno(self, numVenta):
+        sql = "select * from VENTAS where numVenta=" + repr(numVenta)
+        try:
+            self.cursor.execute(sql)
+            rep = self.cursor.fetchone()
+            if rep != None:
+                print(
+                    f"{'NúmVenta':10}{'Cod repuesto':20}{'Fecha vent.':12}\
+                  {'Monto vent':12}"
+                )
+                print(
+                    f"{rep[0]:<10}{rep[1]:20}{rep[2].strftime('%d/%m/%Y'):12}\
+                      {rep[3]:<12}"
+                )
+            else:
+                print("Código no existe")
+        except Exception as err:
+            print(err)
